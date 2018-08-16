708398cc0e264287bc488db43973b68a5f077ae9
Remi Taffin <remitaffin@users.noreply.github.com>
2018-08-16 08:32:35 -0400

    Use port in SQLALCHEMY URI

--- brochure/config.py
+++ brochure/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = u'postgresql+psycopg2://{}:{}@{}/{}'.format(
+    SQLALCHEMY_DATABASE_URI = u'postgresql+psycopg2://{}:{}@{}:5432/{}'.format(
         os.environ['POSTGRES_USER'],
         os.environ['POSTGRES_PASS'],
         os.environ.get('POSTGRES_HOST', 'postgresql'),
