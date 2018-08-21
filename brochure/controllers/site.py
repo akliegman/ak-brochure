a962c055a20add7feb89745d0dc80f230c4d448d
Adam Kliegman <akliegman@noodle.com>
2018-08-21 18:33:59 -0400

    Initial template, scss, static build.

--- brochure/controllers/site.py
+++ brochure/controllers/site.py
@@ -1,9 +1,20 @@
+"""Site controller for all pages (for now)."""
+
 from brochure import app
+from flask import render_template
 
-@app.route('/')
-def index():
-  return 'Hello World I guess...'
 
-@app.route('/otherpage')
-def otherpage():
-  return 'Some other page'
+meta_data = {
+    'subject': 'Product Management',
+    'theme-color': '#000000',
+    'google-analytics': 'xxxxxxxxxx',
+}
+
+
+@app.route('/', methods=['GET'])
+def splash_page():
+    """Generic splash page."""
+    meta_data['title'] = 'Sup'
+    meta_data['description'] = 'Hey'
+
+    return render_template('splash/index.html', theme='splash', meta=meta_data)
---
+++ brochure/scss/config.rb
@@ -0,0 +1,7 @@
+http_path = "/"
+css_dir = "../static/css"
+sass_dir = "./"
+environment = :development
+relative_assets = true
+output_style = :compressed
+preferred_syntax = :scss
