f203194dc6896d7d8c18c97001d8a1cca4358ab1
Adam Kliegman <akliegman@noodle.com>
2018-09-25 11:30:53 -0400

    Designing the splash page and doing a bunch of configurations.

--- brochure/controllers/site.py
+++ brochure/controllers/site.py
@@ -1,21 +1,47 @@
 """Site controller for all pages (for now)."""
 
 from brochure import app
+from datetime import datetime
 from flask import render_template
 
 
 meta_data = {
-    'subject': 'Adam Kliegman is a Senior Product Manager in New York, NY.',
     'theme_color': '#2c292d',
     'ga_tracking_id': 'UA-30649284-1',
     'google_site_verification_token': 'c7P0SFEg16UGqJfn8rq6CbtxT84bcPiqDswU4xSgFWA',
 }
 
 
+@app.context_processor
+def current_date():
+    """Make data accessible on any template"""
+    return {'date': datetime.utcnow()}
+
+
 @app.route('/', methods=['GET'])
 def splash_page():
     """Generic splash page."""
     meta_data['title'] = 'Adam Kliegman'
-    meta_data['description'] = 'It \'s me, Goobie!'
+    meta_data['description'] = (
+        'Adam Kliegman is a Senior Product Manager and Creative Technologist in New York, NY.'
+    )
+    return render_template('pages/splash.html', theme='splash', meta=meta_data)
 
-    return render_template('splash/index.html', theme='splash', meta=meta_data)
+
+@app.route('/cv', methods=['GET'])
+@app.route('/resume', methods=['GET'])
+def cv_page():
+    """Resume page."""
+    meta_data['title'] = 'Javascript: Learn How to do awesome things and break everything'
+    meta_data['description'] = ''
+
+    return render_template('pages/cv.html', theme='cv', meta=meta_data)
+
+
+@app.route('/contac', methods=['GET'])
+def contact_page():
+    """Resume page."""
+    meta_data['title'] = 'Javascript: Learn How to do awesome things and break everything'
+    meta_data['description'] = ''
+
+    return render_template('pages/contact.html', theme='contact', meta=meta_data)
